980f984d78dcf3cdb7edba9d705c6b3d4dae847f
mauraaninditakirana <kiranamaura351@gmail.com>
2025-07-05 18:00:25 +0700

    ubah sedikit

--- asisten/templates/header.php
+++ asisten/templates/header.php
@@ -65,7 +65,7 @@ $userPages = ['users.php', 'tambah_user.php', 'edit_user.php'];
                 <li>
                     <a href="users.php" class="<?php echo in_array($currentPage, $userPages) ? $activeClass : $inactiveClass; ?> flex items-center px-4 py-2.5 rounded-md transition-colors duration-200">
                         <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M15 21a6 6 0 00-9-5.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-3-5.197M15 21a9 9 0 00-9-5.197"></path></svg>
-                        <span>Kelola Pengguna</span>
+                        <span>Manajemen Pengguna</span>
                     </a>
                 </li>
             </ul>
